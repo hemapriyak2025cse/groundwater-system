814f941773b797b47c82272e3387958a4f9808ba
hemapriyak2025cse <hemapriya.k2025cse@sece.ac.in>
2026-04-22 19:27:58 +0530

    initial commit

--- groundwater_system/settings.py
+++ groundwater_system/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-change-in-production')
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
